14a07ea5e69acac6c870780da91ebd8e74f65a33
Fan <creeperloverxxx@gmail.com>
2026-09-24 14:59:40 -0500

    Commit inicial

---
+++ src/main/java/com/forjagym/springboot_forjagym/SpringbootForjagymApplication.java
@@ -0,0 +1,13 @@
+package com.forjagym.springboot_forjagym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootForjagymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootForjagymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forjagym/springboot_forjagym/SpringbootForjagymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forjagym.springboot_forjagym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootForjagymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
